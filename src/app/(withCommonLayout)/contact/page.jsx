88012d510757df53d11fa42bb58dd9e9f5e9dfd2
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-15 01:23:53 +0600

    Add image optimization and layout enhancements to Contact page; update global font settings

--- src/app/(withCommonLayout)/contact/page.jsx
+++ src/app/(withCommonLayout)/contact/page.jsx
@@ -1,9 +1,27 @@
+import Image from 'next/image';
 import React from 'react';
+
+export const metadata = {
+    title : 'Contact | Next App',
+}
 
 const ContactPage = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-2 gap-10'>
+            <img
+                src="https://i.ibb.co.com/3m19PvWc/pexels-pixabay-159866.jpg"
+                alt="book"
+                width={500}
+                height={500}
+                className='mx-auto'
+            />
+            <Image
+                src="https://i.ibb.co.com/3m19PvWc/pexels-pixabay-159866.jpg"
+                alt='book'
+                width={500}
+                height={500}
+                className='mx-auto'
+            />
         </div>
     );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-// `${geistSans.variable} ${geistMono.variable} antialiased
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Next App",
@@ -20,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className} >
       <body
         className='bg-slate-300'
       >
