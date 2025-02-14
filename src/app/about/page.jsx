12dadbc8b8ee1a20be5e02d88cf53dbab314e6f7
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-14 19:03:44 +0600

    Add About page with navigation to Dashboard and implement Footer component in layout

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,26 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const AboutPage = () => {
+    const route = useRouter();
+
+    const handleNavigation = () => {
+        route.push('/dashboard')
+        console.log("clicked");
+    };
+
+
     return (
         <div>
-            <h2>About </h2>
+            <h2 className='text-center'>About </h2>
+
+            <button
+                onClick={handleNavigation}
+                className='border-black p-1 bg-gray-400 rounded-lg text-white'
+            >
+                DashBoard
+            </button>
         </div>
     );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+// `${geistSans.variable} ${geistMono.variable} antialiased
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Next App",
@@ -21,10 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className='bg-slate-300'
       >
         <Navbar />
-        {children}
+        <main className="container mx-auto p-4 flex-grow min-h-[75vh]">{children}</main>
+        <Footer />
       </body>
     </html>
   );
